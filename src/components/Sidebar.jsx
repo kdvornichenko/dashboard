b24672f7e48f00c319df5603253cdc34241f0853
kdvornichenko <gglade999@gmail.com>
2022-05-29 17:02:21 +0600

    Ecommers page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext;
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -17,13 +23,13 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu &&  (<>
       <div className='flex justify-between items-center'>
-        <Link to="/" onClick={() => setActiveMenu(false)}
+        <Link to="/" onClick={handleCloseSidebar}
         className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold translate-tight dark:text-white text-slate-900">
           <SiShopware/> <span>Shoppy</span>
         </Link>
         <TooltipComponent content="Menu" position='BottomCenter'>
           <button type='button'
-          onClick={() => setActiveMenu((prevActiveMebu) => !prevActiveMebu)}
+          onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
           className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
             <MdOutlineCancel />
           </button>
@@ -38,7 +44,7 @@ const Sidebar = () => {
             {item.links.map((link) => (
               <NavLink to={`/${link.name}`}
               key={link.name}
-              onClick={() => {}}
+              onClick={handleCloseSidebar}
               className={({ isActive }) => isActive ? activeLink : normalLink}>
 
               {link.icon}
